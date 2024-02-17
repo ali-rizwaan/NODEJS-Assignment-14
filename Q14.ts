6dffd24cd882836230e0c5ebfbab286712d3cb28
Rizwan Ansari <160248118+ali-rizwaan@users.noreply.github.com>
2024-02-16 20:02:16 -0800

    Add files via upload

---
+++ Q14.ts
@@ -0,0 +1,6 @@
+// 14.Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.
+
+let guestArr: string[] = ["Rahat Ansari ", "Faizan Ansari", "Bilal Ansari"];
+guestArr.map((item) =>
+  console.log(`Dear ${item}, You are invited to a Dinner`)
+);
